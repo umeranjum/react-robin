63c9034f8dafe0725976bedb5d9eeeaa34e71c5b
umeranjum <umaranjum.321@gmail.com>
2019-03-02 23:42:38 +0530

    Corrected the errors

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 
-import Navigation from '../Navigation'
+import Navigation from '../Navigation/Navigation'
 
 const App = () => (
   <Router>
