f1bb2e37287b8b995b6e9bb751925f842320588f
umeranjum <umaranjum.321@gmail.com>
2019-03-03 14:10:32 +0530

    Used compose in signUp

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase/Firebase';
 
@@ -100,7 +101,10 @@ class SignUpFormBase extends Component {
   }
 }
 
-const SignUpForm = withRouter(withFirebase(SignUpFormBase));
+const SignUpForm = compose(
+  withRouter,
+  withFirebase,
+)(SignUpFormBase);
 
 const SignUpLink = () => (
   <p>
